f16644dcc894813e0b065a352b0717eed42c9e1f
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-05-06 11:39:00 +0530

    Update TestCaseResultDto.py

--- notesapp/test/TestCaseResultDto.py
+++ notesapp/test/TestCaseResultDto.py
@@ -1,4 +1,5 @@
 import json
+
 class TestCaseResultDto(dict):
     methodName = ""
     methodType = ""
@@ -7,7 +8,7 @@ class TestCaseResultDto(dict):
     status = ""
     isMandatory = True
     erroMessage = ""
-
+    
     def __init__(self, methodName, methodType, actualScore, earnedScore, status, isMandatory, erroMessage):
-        dict.__init__(self, methodName = methodName, methodType = methodType, actualScore = actualScore, earnedScore = earnedScore, status = status, isMandatory = isMandatory, erroMessage = erroMessage)
-
+        dict.__init__(self, methodName=methodName, methodType=methodType, actualScore=actualScore, 
+                     earnedScore=earnedScore, status=status, isMandatory=isMandatory, erroMessage=erroMessage)
